afdc12e43b408af15e450c133580203c6823e7c9
Alex Li <alex.li@bluerivertech.com>
2023-08-21 10:33:06 -0700

    Ok

--- data/fetch_pp_artifacts.sh
+++ data/fetch_pp_artifacts.sh
@@ -11,6 +11,4 @@ cd /home/${USER}/git/JupiterCVML/europa/base/src/europa/dl/dataset
 # python fetch_pp_artifacts.py \
 # --output-path ${DATASET_PATH}/rev2_data_stratified \
 # --master-csv-s3-uri s3://blueriver-jupiter-data/pack_perception/ml/3ea359f0c3fd28d093bf41b44a016d15_baa7754f69c9985b03cb04a52f5b5bcd/64cae39a0a0438ef306c214d_master_annotations.csv
-python fetch_pp_artifacts.py \
---output-path ${DATASET_PATH}/rev1_data_stratified \
---master-csv-s3-uri s3://blueriver-jupiter-data/pack_perception/ml/5f3ec76b982566fd6e1a1ba6482330ce_e9102e029ccca1fdad1f8dbf60030281/64cadef114aabd7095871141_master_annotations.csv
+python fetch_pp_artifacts.py --output-path ${DATASET_PATH}/Jupiter_train_v5_11 --df-path $DATASET_PATH/master_annotations_30k.csv
