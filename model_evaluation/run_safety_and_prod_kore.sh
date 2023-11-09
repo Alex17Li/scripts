dc7f6a6524025bf9c9d3707bca1d40dcce11123f
Alex Li <alex.li@bluerivertech.com>
2023-11-09 10:14:44 -0800

    Update

--- model_evaluation/run_safety_and_prod_kore.sh
+++ model_evaluation/run_safety_and_prod_kore.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=test_model
 #SBATCH --output=/home/%u/logs/%A_%x
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-gpu=6
-#SBATCH --mem-per-gpu=20G
 #SBATCH --time=10:00:00
 
 # Setup environment variables
@@ -28,23 +26,23 @@ SAFETY_DATASETS=("humans_on_path_test_set_2023_v15_anno")
 for DATASET in ${SAFETY_DATASETS[@]}
 do
     echo ----------------------------RUN ON ${DATASET}-----------------------------------
-    python -m kore.scripts.predict_seg \
+    srun --kill-on-bad-exit python -m kore.scripts.predict_seg \
         --data.test_set.csv_name master_annotations.csv \
         --data.test_set.dataset_path /data/jupiter/datasets/$DATASET \
         --inputs.label.label_map_file $LABEL_MAP_FILE \
         --ckpt_path $CHECKPOINT_FULL_PATH \
         --output_dir ${CHECKPOINT_FULL_DIR}/${DATASET} \
         --states_to_save 'human_false_negative' \
-        --batch_size 64
+        --batch_size 32
 done
 echo ----------------------------RUN_SAFETY_COMPLETE-----------------------------------
 
-PROD_DATASETS=("Jupiter_productivity_test_2023_v1_cleaned" "Jupiter_productivity_test_spring_2023_v2_cleaned" "Jupiter_productivity_airborne_debris_first_night")
-# PROD_DATASETS=()
+# PROD_DATASETS=("Jupiter_productivity_test_2023_v1_cleaned" "Jupiter_productivity_test_spring_2023_v2_cleaned" "Jupiter_productivity_airborne_debris_first_night")
+PROD_DATASETS=()
 for DATASET in ${PROD_DATASETS[@]}
 do
     echo ----------------------------RUN ON ${DATASET}-----------------------------------
-    python -m kore.scripts.predict_seg \
+    srun --kill-on-bad-exit python -m kore.scripts.predict_seg \
         --data.test_set.csv_name master_annotations.csv \
         --data.test_set.dataset_path /data/jupiter/datasets/$DATASET \
         --inputs.label.label_map_file $LABEL_MAP_FILE \
@@ -52,7 +50,7 @@ do
         --output_dir ${CHECKPOINT_FULL_DIR}/${DATASET} \
         --states_to_save 'human_false_negative' \
         --metrics.run-productivity-metrics \
-        --batch_size 64
+        --batch_size 32
 done
 
 echo --------------------------RUN_PRODUCTIVITY_COMPLETE-------------------------------
--- training/train_7_class_kore.sh
+++ training/train_7_class_kore.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 #SBATCH --job-name=seg_dust
 #SBATCH --output=/home/%u/logs/%A_%x
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
 #SBATCH --partition=gpu
-#SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-gpu=8
 #SBATCH --time=150:00:00
+
 source /home/$USER/.bashrc
 conda activate cvml
 
 cd /home/$USER/git
 
-export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-export WORLD_SIZE=$SLURM_NTASKS_PER_NODE
-echo "MASTER_PORT"=$MASTER_PORT
-echo "WORLD_SIZE="$WORLD_SIZE
-
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
-echo "MASTER_ADDR="$MASTER_ADDR
-
 export NCCL_NSOCKS_PERTHREAD=4
 export NCCL_SOCKET_NTHREADS=2
 export NCCL_MIN_CHANNELS=32
+export COLUMNS=100
 
 EXP=${SLURM_JOB_ID}
 
-python -m JupiterCVML.kore.scripts.train_seg \
-    --config_path scripts/kore_configs/harvest_seg_train.yml \
-    --trainer.logger.version $EXP
+CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml"
+CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml scripts/kore_configs/seg_gsam.yml"
 
+set -x
+srun --kill-on-bad-exit python -m JupiterCVML.kore.scripts.train_seg \
+    --config_path $CONFIG_PATH \
+    --data.train_set.csv_name master_annotations_10k.csv \
+    --trainer.logger.version $EXP
 
