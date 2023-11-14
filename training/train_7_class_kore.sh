109011bcd28368c017375da6d25f17d85bb817c1
Alex Li <alex.li@bluerivertech.com>
2023-11-14 06:14:14 -0800

    eh

--- training/train_7_class_kore.sh
+++ training/train_7_class_kore.sh
@@ -18,18 +18,21 @@ export NCCL_MIN_CHANNELS=32
 export COLUMNS=100
 
 EXP=${SLURM_JOB_ID}
+# EXP=16686
+# CKPT_PATH=/mnt/sandbox1/alex.li/wandb/run-20231110_200223-16686/files/epoch\=35-val_loss\=0.113822.ckpt
 
 set -x
+    # --ckpt_path $CKPT_PATH \
 
-# CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml"
-# srun --kill-on-bad-exit python -m JupiterCVML.kore.scripts.train_seg \
-#     --config_path $CONFIG_PATH \
-#     --lr_scheduler.T_max 100 \
-#     --trainer.logger.version $EXP
-
-
-CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml scripts/kore_configs/seg_gsam.yml"
+CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml"
 srun --kill-on-bad-exit python -m JupiterCVML.kore.scripts.train_seg \
     --config_path $CONFIG_PATH \
-    --data.train_set.csv_name master_annotations_10k.csv \
+    --lr_scheduler.T_max 100 \
     --trainer.logger.version $EXP
+
+
+# CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml scripts/kore_configs/seg_gsam.yml"
+# srun --kill-on-bad-exit python -m JupiterCVML.kore.scripts.train_seg \
+#     --config_path $CONFIG_PATH \
+#     --data.train_set.csv_name master_annotations_10k.csv \
+#     --trainer.logger.version $EXP
