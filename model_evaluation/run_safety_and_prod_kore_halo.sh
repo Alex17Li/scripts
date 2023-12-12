f6fb13726060f00cd997655914a90af1debbee2b
Alex Li <alex.li@bluerivertech.com>
2023-12-12 10:13:08 -0800

    update

--- model_evaluation/run_safety_and_prod_kore_halo.sh
+++ model_evaluation/run_safety_and_prod_kore_halo.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=test_model
 #SBATCH --output=/home/%u/logs/%A_%x
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-gpu=6
 #SBATCH --time=10:00:00
 
@@ -16,27 +16,40 @@ DUST_OUTPUT_PARAMS='{"dust_head_output":false}'
 LABEL_MAP_FILE=\$EUROPA_DIR/dl/config/label_maps/eight_class_train_dust_light_as_sky_birds_as_driveable.csv
 # CHECKPOINT_FULL_DIR=/mnt/sandbox1/alex.li/wandb/run-17680/files/
 # CHECKPOINT=last.ckpt
-CHECKPOINT_FULL_DIR=/home/alex.li/logs
-CHECKPOINT=17902.ckpt
+# CHECKPOINT_FULL_DIR=/home/alex.li/logs
+# CHECKPOINT=17902.ckpt
+# OUTPUT_MODEL_NAME=17902
+
+CHECKPOINT_FULL_DIR=/mnt/sandbox1/pooja.mehta/jupiter/models/pmehta_2023/rgb_baseline_sample_a_v3_birds_driveable_train_v6_0_1130
+CHECKPOINT=pmehta_2023_val_bestmodel.pth
+OUTPUT_MODEL_NAME=pmehta_2023_val_bestmodel
+
+# CHECKPOINT_FULL_DIR=/mnt/sandbox1/alex.li/logs/driveable_terrain_model/example_halo_rgb
+# CHECKPOINT=driveable_terrain_model_99_epoch_model.pth
+# OUTPUT_MODEL_NAME=driveable_terrain_model_99_epoch_model
 
 CHECKPOINT_FULL_PATH=${CHECKPOINT_FULL_DIR}/${CHECKPOINT}
 
-echo --------------TESTING MODEL $CHECKPOINT_FULL_PATH---------------------------
+# echo --------------TESTING MODEL $CHECKPOINT_FULL_PATH---------------------------
 
-SAFETY_DATASETS=("halo_rgb_stereo_test_v6_0")
-for DATASET in ${SAFETY_DATASETS[@]}
-do
-    echo ----------------------------RUN ON ${DATASET}-----------------------------------
-    srun --kill-on-bad-exit python -m kore.scripts.predict_seg \
-        --inputs.input_mode RECTIFIED_RGB \
-        --config_path kore/configs/options/seg_no_dust_head.yml \
-        --data.test_set.csv_name master_annotations.csv \
-        --data.test_set.dataset_path /data2/jupiter/datasets/$DATASET \
-        --inputs.label.label_map_file $LABEL_MAP_FILE \
-        --ckpt_path $CHECKPOINT_FULL_PATH \
-        --metrics.use_depth_threshold true \
-        --output_dir ${CHECKPOINT_FULL_DIR}/${DATASET} \
-        --states_to_save 'human_false_negative' 'human_false_positive' \
-        --batch_size 32
-done
+# srun --kill-on-bad-exit python -m kore.scripts.predict_seg \
+#     --config_path kore/configs/options/seg_no_dust_head.yml kore/configs/options/halo_seg_prediction.yml \
+#     --data.test_set.csv_name master_annotations_full_label.csv \
+#     --data.test_set.dataset_path /data2/jupiter/datasets/halo_rgb_stereo_test_v6_0 \
+#     --inputs.label.label_map_file $LABEL_MAP_FILE \
+#     --ckpt_path $CHECKPOINT_FULL_PATH \
+#     --metrics.gt_stop_classes_to_consider 'Non-driveable' 'Trees_Weeds' 'Humans' 'Vehicles' \
+#     --output_dir /mnt/sandbox1/alex.li/introspection/$OUTPUT_MODEL_NAME/halo_rgb_stereo_test_v6_0 \
+#     --states_to_save 'human_false_negative' 'false_negative' 'vehicle_false_negative'
+# echo ----------------------------RUN_PRODUCTIVITY_COMPLETE-----------------------------------
+
+srun --kill-on-bad-exit python -m kore.scripts.predict_seg \
+    --config_path kore/configs/options/seg_no_dust_head.yml kore/configs/options/halo_seg_prediction.yml \
+    --data.test_set.csv_name master_annotations.csv \
+    --data.test_set.dataset_path /data2/jupiter/datasets/halo_humans_on_path_v3 \
+    --inputs.label.label_map_file $LABEL_MAP_FILE \
+    --ckpt_path $CHECKPOINT_FULL_PATH \
+    --output_dir /mnt/sandbox1/alex.li/introspection/$OUTPUT_MODEL_NAME/halo_humans_on_path_v3 \
+    --states_to_save 'human_false_negative' 
 echo ----------------------------RUN_SAFETY_COMPLETE-----------------------------------
+    # --metrics.gt_stop_classes_to_consider Humans \
--- training/train_7_class_kore_halo.sh
+++ training/train_7_class_kore_halo.sh
@@ -10,7 +10,7 @@
 source /home/$USER/.bashrc
 conda activate cvml
 
-cd /home/$USER/git
+cd /home/$USER/git/JupiterCVML
 
 export NCCL_NSOCKS_PERTHREAD=4
 export NCCL_SOCKET_NTHREADS=2
@@ -22,6 +22,6 @@ EXP=${SLURM_JOB_ID}
 set -x
 
 CONFIG_PATH="scripts/kore_configs/harvest_seg_train.yml"
-srun --kill-on-bad-exit python -m JupiterCVML.kore.scripts.train_seg \
+srun --kill-on-bad-exit python -m kore.scripts.train_seg \
     --config_path \$CVML_DIR/kore/configs/defaults/halo_seg_training_params.yml \$CVML_DIR/kore/configs/options/seg_no_dust_head.yml \
     --trainer.logger.version $EXP
